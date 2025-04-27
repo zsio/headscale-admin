022feec279be99af90d90b9e4f904922f615de80
硕 张 <200676@gmail.com>
2025-04-27 11:33:32 +0800

    chore: 更新Next.js版本至15并优化首页重定向逻辑
    将Next.js依赖从^14升级至^15，确保使用最新稳定版本。同时，优化首页重定向逻辑，使用`navigation.redirect`替代`permanentRedirect`，提升代码简洁性和可维护性。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image'
-import {permanentRedirect} from "next/navigation";
+import navigation from "next/navigation";
 
 
 export default function Home() {
-  permanentRedirect("/admin/machines")
   
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      123123
-    </main>
-  )
+  
+  navigation.redirect("/admin/machines")
+
 }
